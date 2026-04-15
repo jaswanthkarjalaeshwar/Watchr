48b6d5af0c6dba17f16a2a4d68b0249f6c53b24a
Jaswanth Karjala Eshwar <jaswanthkarjalaeshwar@gmail.com>
2026-04-15 00:09:18 -0400

    clean up test files

--- inject_test.py
+++
@@ -1,4 +0,0 @@
-from database import save_snapshot 
-import hashlib 
-fake = 'Notion pricing updated: Plus plan now $16/month, up from $12. Team plan $20/user.' 
-save_snapshot('Notion', 'pricing', 'https://www.notion.so/pricing', fake, hashlib.md5(fake.encode()).hexdigest()) 
